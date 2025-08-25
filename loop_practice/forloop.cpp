55563113fe22d98f5fd513b7a0a56b50c73cc634
Aditya Prasad <dypatil.aditya00@gmail.com>
2025-08-25 19:47:01 +0530

    Update forloop.cpp

--- loop_practice/forloop.cpp
+++ loop_practice/forloop.cpp
@@ -1,1 +1,10 @@
-
+#include<iostream>
+using namespace std;
+int main() {
+int month;
+    cout<<"Enter Month : ";
+   for (cin>>month ; month <= 12 ; month++) {
+       cout<<month<<endl;
+   }
+    return 0;
+}
