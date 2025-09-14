09819af8645b4e1ec6705c5de7fb611f12aad059
Aditya Prasad <dypatil.aditya00@gmail.com>
2025-09-14 18:24:37 +0530

    Add files via upload
    take input of array size and the elements form user

---
+++ array/arrinput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << " elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "You entered: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
